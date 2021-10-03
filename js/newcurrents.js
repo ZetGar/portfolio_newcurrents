5f171fde46513d90f1dc326cc7da745881805105
ZetGar <88043986+ZetGar@users.noreply.github.com>
2021-10-03 14:35:42 +0900

    Create all.css

--- js/newcurrents.js
+++ js/newcurrents.js
@@ -27,10 +27,91 @@ $(function () {
   });
 
   // scrollTop
-  const arrTopVal = [];
-  for (let i = 0; i < 5; i++) {
-    arrTopVal[i] = $("section").eq(i).offset().top;
+  // const arrTopVal = [];
+  // for (let i = 0; i < 5; i++) {
+  //   arrTopVal[i] = $("section").eq(i).offset().top();
+  // }
+
+  // console.log(arrTopVal);
+
+  // button
+  const $btn = document.querySelectorAll(".btn");
+  $btn.forEach((btn) => {
+    btn.addEventListener("mouseenter", function (e) {
+      let x = e.clientX - e.target.offsetLeft;
+      let y = e.clientY - e.target.offsetTop;
+
+      let ripples = document.createElement("span");
+      ripples.style.left = x + "px";
+      ripples.style.top = y + "px";
+      this.appendChild(ripples);
+
+      setTimeout(() => {
+        ripples.remove();
+      }, 1000);
+    });
+  });
+
+  // class
+  let online = [
+    {
+      id: 0,
+      img: "class1.png",
+      title: "먹방, 하루 만에 해치우기",
+      price: 99000,
+    },
+    {
+      id: 1,
+      img: "class2.png",
+      title: "디지털마케팅과 페이스북/인스타그램 광고",
+      price: 69000,
+    },
+    {
+      id: 2,
+      img: "class3.png",
+      title: "오프라인 매장, 매출 향상을 위한 온라인 마케팅",
+      price: 132000,
+    },
+    {
+      id: 3,
+      img: "class4.png",
+      title: "매출 향상을 위한 온라인 쇼핑몰 마케팅",
+      price: 132000,
+    },
+    {
+      id: 4,
+      img: "class5.png",
+      title: "쉽게 배우고 알차게 써먹는 영상 촬영의 기본 공식",
+      price: 132000,
+    },
+    {
+      id: 5,
+      img: "class6.png",
+      title: "전문적인 영상 제작을 위한 유튜브 콘텐츠 편집",
+      price: 99000,
+    },
+    {
+      id: 6,
+      img: "class7.png",
+      title: "스마트폰 하나로 뚝딱! 손 등급 한번에 올리기",
+      price: 9900,
+    },
+  ];
+
+  for (let i = 0; i < online.length; i++) {
+    $(".card_img")
+      .eq(i)
+      .css({
+        backgroundImage: `url(./images/${online[i].img})`,
+      });
+
+    $(".cardtit").eq(i).html(online[i].title);
+    $(".cardprice")
+      .eq(i)
+      .html("₩ " + online[i].price);
   }
 
-  console.log(arrTopVal);
+  const $clSlide = $(".cl_slide");
+
+  $clSlide.on("load", function () {});
 });
